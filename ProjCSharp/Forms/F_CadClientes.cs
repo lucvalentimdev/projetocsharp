0f2dcceb4956cb6fb870cd56c14bd1993a1cd7a3
Lucas Valentim Developer <109736052+lucvalentimdev@users.noreply.github.com>
2024-04-07 22:38:11 -0300

    Commit_2 07042024
    Novas implementacoes

--- ProjCSharp/Forms/F_CadClientes.cs
+++ ProjCSharp/Forms/F_CadClientes.cs
@@ -4,8 +4,6 @@ namespace SalSystem.Views
 {
     public partial class F_CadClientes : Form
     {
-        readonly Utilities utilities = new();
-
         public F_CadClientes()
         {
             InitializeComponent();
@@ -24,12 +22,13 @@ namespace SalSystem.Views
         private async Task GetCEP(string _cep)
         {
             using var cepService = new CepService();
-            var (City, State) = await cepService.GetCityAndStateByCepAsync(_cep);
 
+            var (City, State) = await cepService.GetCityAndStateByCepAsync(_cep);
             if (!string.IsNullOrEmpty(City))
             {
                 txtCidade.Text = City;
                 cboUF.Text = State;
+                txtLogradouro.Focus();
             }
             else
                 MessageBox.Show("Ocorreu um erro ao consultar o CEP!", "ERRO!");
@@ -37,12 +36,12 @@ namespace SalSystem.Views
 
         private void TxtCEP_KeyPress(object sender, KeyPressEventArgs e)
         {
-            utilities.NumbersOnly(e);
+            Utilities.NumbersOnly(e);
         }
 
         private void textBox7_KeyPress(object sender, KeyPressEventArgs e)
         {
-            utilities.NumbersOnly(e);
+            Utilities.NumbersOnly(e);
         }
     }
 }
--- ProjCSharp/Services/Utilities.cs
+++ ProjCSharp/Services/Utilities.cs
@@ -2,7 +2,6 @@
 {
     internal class Utilities
     {
-        
         public static void NumbersOnly(KeyPressEventArgs e)  // Igonara teclas que não forem número //Como parametro recebe (e) do evento KeyPress // 
         {                                                         
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
